c8f4402cfeeaf7a75edd93b74f3ae277997d94fa
vaidehisharan <137915438+vaidehisharan@users.noreply.github.com>
2023-06-28 09:56:51 +0530

    Update streamlit_app.py
    Added another list of food

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Niner')
+
+streamlit.title('Breakfast menu')
+streamlit.title('omega 3 & blueberry oatmeal')
+streamlit.title('kale, spinach & Rocket Smoothie')
+streamlit.title('Hard boiled Free-Range Egg')
