0627fa853f2a13e7e5b5e980a0daddb832f1c8bd
vaidehisharan <137915438+vaidehisharan@users.noreply.github.com>
2023-06-29 21:01:49 +0530

    Update streamlit_app.py
    error try catch

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,12 @@ streamlit.dataframe(fruits_to_show)
 
 # New Section to display fruityvice API Response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','banana')
+try:
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
+if not fruit_choice:
 streamlit.write('The user entered ', fruit_choice)
+else:
+
 
 # import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
@@ -35,6 +39,9 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # frame the fruityvice data into the table
 streamlit.dataframe(fruityvice_normalized)
+
+except URLError as e:
+streamlit.error()
 
 #Stop the duplicate
 streamlit.stop()
