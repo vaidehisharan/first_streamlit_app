1b2f65f09a97baf79e8d5bf6ef865fd31eb18e3b
vaidehisharan <137915438+vaidehisharan@users.noreply.github.com>
2023-06-29 20:30:38 +0530

    Update streamlit_app.py
    added another block

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,3 +39,8 @@ my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("the fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+# New Section to display fruityvice API Response
+streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','jackfruit')
+streamlit.write('The user entered ', fruit_choice)
