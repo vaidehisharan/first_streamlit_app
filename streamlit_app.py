e74dbaa7428d741084c1e67b4d20daac427d4007
vaidehisharan <137915438+vaidehisharan@users.noreply.github.com>
2023-06-30 10:51:18 +0530

    Update streamlit_app.py
    kjj

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,12 +36,12 @@ try:
   
   else:
   
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  streamlit.dataframe(fruityvice_normalized)
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      streamlit.dataframe(fruityvice_normalized)
 
 except URLError as e:
-streamlit.error()
+    streamlit.error()
 
 #Stop the duplicate
 streamlit.stop()
